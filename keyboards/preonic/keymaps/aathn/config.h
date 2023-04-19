4333153c0ba9cf668546d3764b5fc4de07940d91
Anders Ågren Thuné <anders@aathn.org>
2023-04-19 23:59:52 +0200

    Update preonic layout to match ergodox and sync QMK changes

--- keyboards/preonic/keymaps/aathn/config.h
+++ keyboards/preonic/keymaps/aathn/config.h
@@ -48,3 +48,13 @@
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+/*
+ * Tapping settings
+ */
+
+#define TAPPING_TERM 200
+
+#define PERMISSIVE_HOLD
+
+#define IGNORE_MOD_TAP_INTERRUPT
